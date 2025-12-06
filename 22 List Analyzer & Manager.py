062c655ef2d1a16afb255c6c5e02aafafe309395
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-12-06 13:03:53 +0530

    Add files via upload

---
+++ 22 List Analyzer & Manager.py
@@ -0,0 +1,32 @@
+days_of_week = ["mon","tue","wed","thu","fri","sat","sun"]
+print(days_of_week)
+days_of_week.reverse()
+print(days_of_week)
+
+nums = [4, 9, 0, 1, 2, 8]
+print(nums)
+nums.sort()
+print(nums)
+
+nums.sort(reverse=True)
+print(nums)
+
+numbers = [0, 1, 3, 4, 1, 0, 5, 0, 0, 3, 0]
+print(numbers)
+
+print(numbers.count(0))
+
+number = int(input("enter number to count: "))
+print(numbers.count(number))
+
+language = ["python", "java", "c++", "python"]
+print(language)
+
+lan = input("enter language to be count: ")
+print(language.count(lan))
+
+print("python" in language)
+print("java script" in language)
+print("python" in language)
+print("python " in language)
+
