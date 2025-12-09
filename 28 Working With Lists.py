6d2803281fc602139409981c65a45142d477ea87
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-12-09 13:00:32 +0530

    Add files via upload

---
+++ 28 Working With Lists.py
@@ -0,0 +1,18 @@
+l1 = [5, 1.5, "python", True, None, [1, 2, 3], 10]
+print("=== Part 1: Single List (l1) ===")
+print(l1)
+print(len(l1))
+print(l1[-2])
+print(l1[-2][0])
+
+l2 =[[1, 2], [3, 4], [5, 6, [0, 1]]]
+print("=== Part 2: Nested List (l2) ===")
+print(l2)
+print(len(l2))
+print(l2[-1])
+print(l2[-1][-1][-1])
+print("=== Project Completed ===")
+
+
+
+
