5c69f8150f350ca6d3404ef2b72d268896d7c14b
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-12-27 20:03:33 +0530

    Update 34 mutability vs immutability in python.py

--- 34 mutability vs immutability in python.py
+++ 34 mutability vs immutability in python.py
@@ -1,45 +1,27 @@
-print("=== STRING IMMUTABILITY ===")
 s1 = "python is fun"
-print(s1)
-s2 = s1.replace("python","java")
+s2 = s1.replace("python", "java")
 print(s1)
 print(s2)
+s3 = s1.capitalize()
+print(s3)
 
-try:
-    s1[0]="p"
-    print(s1)
-except:
-    print("error")
-print("=== TUPLE IMMUTABILITY ===")
 t1 = ("mango", "orange", "apple")
-try:
-    t1.append("banana")
-    print(t1)
-except:
-    print("error")
+l = list(t1)
+l.append("banana")
+l[0] = "banana"
 
-try:
-    t1[0] = "banana"
-except:
-    print("error")
-print("=== LIST MUTABILITY ===")
+
 l1 = ["mango", "orange", "apple"]
-print(l1)
-l1.append("banana")
-print(l1)
-print("=== LIST MEMORY BEHAVIOR ===")
-l2 = ["mango", "orange", "apple"]
-print(l2)
-print(id(l2))
-l2.append("banana")
-print(l2)
-print(id(l2))
-print("=== MODIFYING LIST ELEMENTS ===")
-l3 = ["mango", "orange", "apple"]
-print(l3[-1])
-print(id(l3))
-l3[-1] = "apple"
-print(l3)
-print(id(l3))
-print("=== END OF PROJECT ===")
+print(id(l1))
+l.append("banana")
+print(id(l1))
+l1[-1] = "grapes"
+
+print("""
+| Data Type | Mutable? | Can Change Elements? | Memory Behavior |
+| --------- | -------- | -------------------- | --------------- |
+| String    | no        | no                   | different    |
+| Tuple     | no        | no                    | different    |
+| List      | yes       | yes                    | same         |
+""")
 
